153c182179da35939f8a37f576450fe068428588
micha <micha@MICHAEL>
2024-10-17 13:45:40 +0200

    Add project files.

---
+++ OpenGL_Project/Game.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace OpenGL_Project
+{
+    internal class Game : GameWindow
+    {
+        float[] vertices = {
+            -0.5f, -0.5f, 0.0f, //Bottom-left vertex
+             0.5f, -0.5f, 0.0f, //Bottom-right vertex
+             0.0f,  0.5f, 0.0f  //Top vertex
+        };
+
+        public Game(int width, int height, string title) : base(GameWindowSettings.Default, new NativeWindowSettings() { Size = (width, height), Title = title }) 
+        {
+            
+        }
+
+        protected override void OnLoad()
+        {
+            base.OnLoad();
+
+            GL.ClearColor(0.05f, 0.05f, 0.05f, 1.0f);
+
+            //Code goes here
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            //Code goes here.
+
+            SwapBuffers();
+        }
+
+        protected override void OnFramebufferResize(FramebufferResizeEventArgs e)
+        {
+            base.OnFramebufferResize(e);
+
+            GL.Viewport(0, 0, e.Width, e.Height);
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+
+            if (KeyboardState.IsKeyDown(Keys.Escape))
+            {
+                Close();
+            }
+        }
+    }
+}
---
+++ OpenGL_Project/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace OpenGL_Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Game game = new Game(800, 600, "LearnOpenTK"))
+            {
+                game.Run();
+            }
+        }
+    }
+}

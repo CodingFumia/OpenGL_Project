59f2c911cb3ba99f2376d6a5753bababcbf3e4f2
xFuzzyYT <michael7773105@gmail.com>
2025-05-24 19:45:17 +0200

    Render Rainbow Rectangle

--- OpenGL_Project/Game.cs
+++ OpenGL_Project/Game.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using OpenTK.Graphics.OpenGL4;
 using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
@@ -10,35 +6,81 @@ using OpenTK.Windowing.GraphicsLibraryFramework;
 
 namespace OpenGL_Project
 {
-    internal class Game : GameWindow
+    public class Game : GameWindow
     {
-        float[] vertices = {
-            -0.5f, -0.5f, 0.0f, //Bottom-left vertex
-             0.5f, -0.5f, 0.0f, //Bottom-right vertex
-             0.0f,  0.5f, 0.0f  //Top vertex
+        private readonly float[] vertices = {
+             0.5f,  0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // top right        color
+             0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  // bottom right
+            -0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f,  // bottom left
+            -0.5f,  0.5f, 0.0f,  1.0f, 0.1f, 0.9f   // top left
         };
+
+        uint[] indices = {  // note that we start from 0!
+            0, 1, 3,   // first triangle
+            1, 2, 3    // second triangle
+        };
+
+        Shader shader;
+
+        int VertexBufferObject;
+        int VertexArrayObject;
+        int ElementBufferObject;
 
         public Game(int width, int height, string title) : base(GameWindowSettings.Default, new NativeWindowSettings() { Size = (width, height), Title = title }) 
         {
-            
+
         }
 
         protected override void OnLoad()
         {
             base.OnLoad();
-
             GL.ClearColor(0.05f, 0.05f, 0.05f, 1.0f);
 
-            //Code goes here
+            VertexArrayObject = GL.GenVertexArray();
+            GL.BindVertexArray(VertexArrayObject);
+
+            VertexBufferObject = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject);
+            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
+
+            ElementBufferObject = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, ElementBufferObject);
+            GL.BufferData(BufferTarget.ElementArrayBuffer, indices.Length * sizeof(uint), indices, BufferUsageHint.StaticDraw);
+
+            shader = new Shader("Shaders/shader.vert", "Shaders/shader.frag");
+            shader.Use();
+
+            // OLD ONLY POSITION
+            //GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+            //GL.EnableVertexAttribArray(0);
+
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 6 * sizeof(float), 0);
+            GL.EnableVertexAttribArray(0);
+
+            GL.VertexAttribPointer(1, 3, VertexAttribPointerType.Float, false, 6 * sizeof(float), 3 * sizeof(float));
+            GL.EnableVertexAttribArray(1);
+        }
+
+        protected override void OnUnload()
+        {
+            base.OnUnload();
+
+            shader.Dispose();
+            GL.DeleteBuffer(VertexBufferObject);
+            GL.DeleteVertexArray(VertexArrayObject);
         }
 
         protected override void OnRenderFrame(FrameEventArgs e)
         {
             base.OnRenderFrame(e);
-
             GL.Clear(ClearBufferMask.ColorBufferBit);
 
-            //Code goes here.
+            shader.Use();
+
+            // now render the element
+            GL.BindVertexArray(VertexArrayObject);
+            GL.DrawElements(PrimitiveType.Triangles, indices.Length, DrawElementsType.UnsignedInt, 0);
+            //GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
 
             SwapBuffers();
         }
--- OpenGL_Project/Program.cs
+++ OpenGL_Project/Program.cs
@@ -1,6 +1,6 @@
 ﻿namespace OpenGL_Project
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
